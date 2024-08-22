bcc67922d4ff2802886861064e56fa9028079f85
mayssam73 <mayssamkalajo@gmail.com>
2024-08-22 10:00:55 -0500

    Added resources from video and code to store stock prices in MinIO

--- dags/stock_market.py
+++ dags/stock_market.py
@@ -5,7 +5,7 @@ from airflow.operators.python import PythonOperator
 from datetime import datetime
 import requests
 
-from include.stock_market.tasks import _get_stock_prices
+from include.stock_market.tasks import _get_stock_prices, _store_prices
 
 SYMBOL = 'AAPL'
 
@@ -30,7 +30,13 @@ def stock_market():
         python_callable = _get_stock_prices,
         op_kwargs = {'url': '{{ task_instance.xcom_pull(task_ids="is_api_available")}}', 'symbol': SYMBOL}
     )
+
+    store_prices = PythonOperator(
+        task_id = 'store_prices',
+        python_callable =_store_prices,
+        op_kwargs = {'stock': '{{ task_instance.xcom_pull(task_ids="get_stock_prices")}}'}
+    )
     
-    is_api_available() >> get_stock_prices
+    is_api_available() >> get_stock_prices >> store_prices
 
 stock_market()
